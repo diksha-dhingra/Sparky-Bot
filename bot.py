5402ff0cf9b32ebc564a8fc5924ea4ac938b3b68
diksha-dhingra <dikshadhingra261@gmail.com>
2026-03-31 14:42:38 +0530

    model updated

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from personality import SYSTEM_PROMPT
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash",
+    model_name="gemini-1.5-flash"
     system_instruction=SYSTEM_PROMPT
 )
 
